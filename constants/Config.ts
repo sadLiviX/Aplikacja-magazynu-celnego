e9381fe5d554faadbb39e6f11f56b893f6f07c22
Jakub <kubad63@gmail.com>
2026-06-10 22:18:01 +0200

    wgranie backupu

--- constants/Config.ts
+++ constants/Config.ts
@@ -1,2 +1,3 @@
 // Tutaj zmieniamy IP na takie jak ma kompik, bo jak się przenosi plik po innych lapkach to json serwer nie będzie banglał
+// json serwer uruchamiamy komendą: json-server --watch db.json --port 3000
 export const API_URL = 'http://10.69.64.118:3000';
