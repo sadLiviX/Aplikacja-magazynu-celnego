422bd9a13f62d49e52725eb44a4f1d3f902a5633
Jakub Dabrowski <kubad63@gmail.com>
2026-06-12 21:25:55 +0200

    Poprawka wyświetlania liczby awb w magazynie

--- constants/Config.ts
+++ constants/Config.ts
@@ -1,3 +1,3 @@
 // Tutaj zmieniamy IP na takie jak ma kompik, bo jak się przenosi plik po innych lapkach to json serwer nie będzie banglał
 // json serwer uruchamiamy komendą: npx json-server --watch db.json --port 3000
-export const API_URL = 'http://10.69.64.118:3000';
+export const API_URL = 'http://192.168.1.16:3000';
